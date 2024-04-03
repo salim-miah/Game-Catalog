250d3f0e41b80a4f467f2ac27f1096259fd00a72
salim-miah <salimmiah080802@gmail.com>
2024-04-03 14:14:45 +0600

    Added a background

--- signup.php
+++ signup.php
@@ -29,16 +29,18 @@
 </head>
 <style>
     #webpage {
-        background-color: #f2f2f2;
+        background-image: url(9AC890F8-C6BB-436F-81B1-F713D2B11D14.png);
+        background-repeat: no-repeat;
+        background-size: cover;
     }
     #signup_section {
-        background-color: white;
+        background-color: rgba(255, 255, 255, 0.579);
         float: right;
         width: 350px;
         min-height: 250px;
         box-shadow:  0px 5px 10px 0px rgba(0, 0, 0, 0.505);
         border-radius: 7px;
-        margin-top: 5px;
+        margin-top: 40px;
         margin-right: 500px;
     }
     #inputs {
@@ -55,17 +57,17 @@
         height:40px;
         border-radius: 4px;
         margin-left: 40px;
-        border: none;
-        background-color: #1a75ff;
+        border: 2px solid white;
+        background-color: rgba(255, 255, 255, 0);
         padding: 5px;
         font-family: Klavika;
         text-align: center;
-        color: white;
+        color: #503C3C;
         font-weight: bold;
         font-size: 20px;
     }
     #Create:hover {
-        background-color: #4dff4d;
+        background-color: rgba(255, 255, 255, 0.705);
     }
 
     .column {
@@ -77,14 +79,14 @@
 <body id="webpage">
     <div>
         <div id="signup_section" class="column">
-            <div style="color: black; font-family: Klavika; text-align: center;font-size: 30px; font-weight: bolder; margin-bottom: 30px;">Sign Up Here</div>
+            <div style="color: #481E14; font-family: Klavika; text-align: center;font-size: 30px; font-weight: bolder; margin-bottom: 30px;">Sign Up Here</div>
             <form method="post">
 
                 <div><input type="text" placeholder="First Name" id="inputs" name="firstname"></div>
                 <br>
                 <div><input type="text" placeholder="Last Name" id="inputs" name="lastname"></div>
                 <br>
-                <span style="margin-left: 40px; font-size: 15px;">Date of Birth</span>
+                <span style="margin-left: 40px; font-size: 15px; color: #481E14;">Date of Birth</span>
                 <div><input type="date" id="inputs" name="dob"></div>
                 <br>
                 <div ><input type="text" placeholder="Email" id="inputs" name="email"></div>
