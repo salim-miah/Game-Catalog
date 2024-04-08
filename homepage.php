fb9b4cb40ae29b035cfd82157f967848170dd260
salim-miah <salimmiah080802@gmail.com>
2024-04-08 21:57:42 +0600

    Renamed file

---
+++ homepage.php
@@ -0,0 +1,39 @@
+<?php
+    
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Homepage | GameChronicles</title>
+    </head>
+
+    <style type="text/css">
+
+        #top_bar{
+            height: 50px;
+            background-color: #92695d;
+            color: #ffffff;
+        }
+    </style>
+
+    <body style="font-family: georgia; background-color: #FFE4D7;">
+        <div id="top_bar">
+            <div style="float: left;font-size: 30px; margin: 8px;">
+                GameChronicles
+            </div>
+            <div style="float: right;font-size: 20px;margin: 10px;">
+                <div>Logged in as, Pranto</div>
+                <!-- <a href="profile.html" target="_blank" style="color: #ffffff">Logged in as, Pranto</a> -->
+            </div>
+        </div>
+            
+        <div style="width: 900px; margin: auto; background-color: black; height: 350px;">
+            <img src="cover.jpg" style="width:900px; height: 350px;">
+        </div>
+        
+
+    </body>
+</html>
