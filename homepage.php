abd041b631e12e05bbadfb6aa8fabec56977ca16
salim-miah <salimmiah080802@gmail.com>
2024-04-09 14:38:13 +0600

    Added logout

--- homepage.php
+++ homepage.php
@@ -4,6 +4,9 @@
     //print_r($_SESSION);
     include("classes/connect.php");
     include("classes/login.php");
+    include("classes/user.php");
+
+    $name="";
 
     //Check if user is logged in
     if(isset($_SESSION['gamelist_userid']) & is_numeric( $_SESSION['gamelist_userid'] ))
@@ -15,7 +18,10 @@
         if($result == true)
         {
             //Retrive user data
-            echo "Everything is fine";
+            $user= new User();
+            $data=$user->user_data($id);
+            $name.=$data['firstname']." ".$data['lastname'];
+            
         }
         else
         {
@@ -52,9 +58,12 @@
                 GameChronicles
             </div>
             <div style="float: right;font-size: 20px;margin: 10px;">
-                <div>Logged in as, Pranto</div>
+                <div>Logged in as, <?php echo $name; ?></div>
                 <!-- <a href="profile.html" target="_blank" style="color: #ffffff">Logged in as, Pranto</a> -->
             </div>
+            <a href="logout.php" style="float: right; margin: 12px; color: #ffffff">
+                <span>Logout</span>
+            </a>
         </div>
             
         <div style="width: 900px; margin: auto; background-color: black; height: 350px;">
