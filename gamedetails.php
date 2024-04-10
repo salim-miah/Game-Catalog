d19396c99cf05495b10ab927ac67014771398f7f
salim-miah <salimmiah080802@gmail.com>
2024-04-10 20:44:17 +0600

    Added file

---
+++ gamedetails.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Game's Details</title>
+</head>
+<body style="font-family: georgia; background-color: #FFE4D7;">
+</body>
+</html>
