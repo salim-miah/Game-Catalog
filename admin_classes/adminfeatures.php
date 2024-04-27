6796438ea97504245c70b73284f8a7ce146abe83
salim-miah <salimmiah080802@gmail.com>
2024-04-27 15:45:38 +0600

    Added functions

--- admin_classes/adminfeatures.php
+++ admin_classes/adminfeatures.php
@@ -2,10 +2,11 @@
 
 class AdminFeatures
 {
-    public function add_game($name,$genre,$date,$dev,$hq,$plat,$synop,$image)
+    public function add_game($name,$genre,$date,$dev,$hq,$synop,$image)
         {
             $query2 = "insert into developers (developer_name, headquarters) values 
             ('$dev', '$hq')";
+            $synop=addslashes($synop);
             $query="insert into games(name, genre, release_date, developer, synopsis, images) values 
             ('$name','$genre','$date','$dev','$synop', '$image')";
             $DB = new Database();
@@ -30,6 +31,23 @@ class AdminFeatures
 
     }
 
+    public function retrieve_gameid($game_name,$game_date,$synopsis)
+    {
+        $synopsis=addslashes($synopsis);
+        $query="select game_id from games where name='$game_name' and release_date='$game_date' and synopsis='$synopsis'";
+        $DB = new Database();
+        $result = $DB->read($query);
+        $row=$result[0];
+        $game_id=$row['game_id'];
+        return $game_id;
+    }
+
+    public function add_platform($game_id,$platform)
+    {
+        $query="insert into platforms(game_id,platform) values ('$game_id','$platform')";
+        $DB = new Database();
+        $result = $DB->save($query);
+    }
     
     public function check_user($id)
     {
